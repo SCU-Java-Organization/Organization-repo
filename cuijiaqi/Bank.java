527a4f3404d7aaf90f45386067d362ed1d7894db
Rachel-Scu <55613873+Rachel-Scu@users.noreply.github.com>
2019-10-06 22:08:24 +0800

    Update Bank.java

--- cuijiaqi/Bank.java
+++ cuijiaqi/Bank.java
@@ -5,23 +5,15 @@ public class Bank {
     int log_in=0;
 
     public void add_accout() {
-        Scanner input = new Scanner(System.in);
-        while (true) {
-            System.out.println("0:exit  1：open an account");
-            int n = input.nextInt();
-            if (n == 0) break;
-            else {
-                System.out.println("Please enter your ID:");
-                String temp = input.nextLine();
-                String id = input.nextLine();
-                Person person1=new Person();
-                person1.setID(id);
-                person1.setMoney(0);
-                person[log_in] = person1;
-                log_in++;
-            }
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter your ID:");
+        String id = input.nextLine();
+        Person person1=new Person();
+        person1.setID(id);
+        person1.setMoney(0);
+        person[log_in] = person1;
+        log_in++;
         }
-    }
 
     public void cash_in(String id,double money){
         for(int i=0;i<log_in;i++){
@@ -45,12 +37,16 @@ public class Bank {
         for (int i = 0; i < log_in; i++) {
             if (person[i].getID().equals(id1)) {
                 person[i].setMoney(person[i].getMoney() - money);
-                if (person[i].getID().equals(id2)) {
-                    person[i].setMoney(person[i].getMoney() + money);
-                    System.out.println("Your current money is: " + person[i].getMoney());
+                System.out.println("Your current money is: " + person[i].getMoney());
+                break;
+            }
+        }
+        for(int j=0;j<log_in;j++){
+                if (person[j].getID().equals(id2)) {
+                    person[j].setMoney(person[j].getMoney() + money);
+                    break;
                 }
             }
 
         }
-    }
 }
